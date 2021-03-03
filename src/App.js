dcbfcbace65ce23ce27bf62f7722a228b540d5c9
unknown <201752008@iiitvadodara.ac.in>
2021-03-03 11:17:02 +0530

    App js change
    Signed-off-by: unknown <201752008@iiitvadodara.ac.in>

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello Everyone!!
+          How are you!!
         </p>
         <a
           className="App-link"
